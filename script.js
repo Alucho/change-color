d918a08f5f3643e48b59d1ddddd2f9c56f0642dc
Anderson Lucho <andersonlucho@hotmail.com>
2022-04-13 12:59:58 -0300

    main

---
+++ script.js
@@ -0,0 +1,44 @@
+function color(){
+    var element = document.getElementById("cor");
+    element.style.backgroundColor="#900"
+}
+
+console.log (new Date().getTime());
+
+{/* <color name="purple_200">#FFBB86FC</color>
+<color name="purple_500">#FF6200EE</color>
+<color name="purple_700">#FF3700B3</color>
+<color name="teal_200">#FF03DAC5</color>
+<color name="teal_700">#FF018786</color>
+<color name="black">#FF000000</color>
+<color name="white">#FFFFFFFF</color>
+<color name="blue">#0000FF</color>
+<color name="green">#008000</color>
+<color name="pink">#FFC0CB</color>
+<color name="red">#FF0000</color>
+<color name="purple">#993399</color>
+<color name="yellow">#FFFF00</color>
+<color name="grey">#D3D3D3</color>
+<color name="lilac">#C8A2C8</color> */}
+
+/*
+val btn = findViewById(R.id.btCliqueAqui) as Button
+btn.setOnClickListener {
+
+    val tpnumero = System.currentTimeMillis()
+    val tpstring = tpnumero.toString()
+    val laststr  = tpstring.last().toString()
+    val root = findViewById(R.id.bgBase) as View
+    val color = when(laststr) {
+        "1" -> getResources().getColor(R.color.black)
+        "2" -> getResources().getColor(R.color.blue)
+        "3" -> getResources().getColor(R.color.green)
+        "4" -> getResources().getColor(R.color.pink)
+        "5" -> getResources().getColor(R.color.red)
+        "6" -> getResources().getColor(R.color.purple)
+        "7" -> getResources().getColor(R.color.yellow)
+        "8"-> getResources().getColor(R.color.grey)
+        "9" -> getResources().getColor(R.color.lilac)
+        else -> getResources().getColor(R.color.white)
+    }
+*/
