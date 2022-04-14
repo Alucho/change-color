76a9a18cf3edbb4d9c78771622fbfef268d5c74c
Anderson Lucho <andersonlucho@hotmail.com>
2022-04-14 09:05:56 -0300

    main

--- script.js
+++ script.js
@@ -1,44 +1,60 @@
-function color(){
-    var element = document.getElementById("cor");
-    element.style.backgroundColor="#900"
-}
+function clicktime(){
 
-console.log (new Date().getTime());
+    var miliSec = Date.now()%10;
+    switch (miliSec) {
+        case 0:
+            var color = document.getElementById("cor");
+            color.style.backgroundColor='#ffffff';
+        break;
 
-{/* <color name="purple_200">#FFBB86FC</color>
-<color name="purple_500">#FF6200EE</color>
-<color name="purple_700">#FF3700B3</color>
-<color name="teal_200">#FF03DAC5</color>
-<color name="teal_700">#FF018786</color>
-<color name="black">#FF000000</color>
-<color name="white">#FFFFFFFF</color>
-<color name="blue">#0000FF</color>
-<color name="green">#008000</color>
-<color name="pink">#FFC0CB</color>
-<color name="red">#FF0000</color>
-<color name="purple">#993399</color>
-<color name="yellow">#FFFF00</color>
-<color name="grey">#D3D3D3</color>
-<color name="lilac">#C8A2C8</color> */}
+        case 1:
+            var color = document.getElementById("cor");
+            color.style.backgroundColor='#000000';
+        break;
 
-/*
-val btn = findViewById(R.id.btCliqueAqui) as Button
-btn.setOnClickListener {
+        case 2:
+            var color = document.getElementById("cor");
+            color.style.backgroundColor='#0000ff';
+        break;
 
-    val tpnumero = System.currentTimeMillis()
-    val tpstring = tpnumero.toString()
-    val laststr  = tpstring.last().toString()
-    val root = findViewById(R.id.bgBase) as View
-    val color = when(laststr) {
-        "1" -> getResources().getColor(R.color.black)
-        "2" -> getResources().getColor(R.color.blue)
-        "3" -> getResources().getColor(R.color.green)
-        "4" -> getResources().getColor(R.color.pink)
-        "5" -> getResources().getColor(R.color.red)
-        "6" -> getResources().getColor(R.color.purple)
-        "7" -> getResources().getColor(R.color.yellow)
-        "8"-> getResources().getColor(R.color.grey)
-        "9" -> getResources().getColor(R.color.lilac)
-        else -> getResources().getColor(R.color.white)
+        case 3:
+            var color = document.getElementById("cor");
+            color.style.backgroundColor='#008000';
+        break;
+
+        case 4:
+            var color = document.getElementById("cor");
+            color.style.backgroundColor='#ffc0cb';
+        break;
+
+        case 5:
+            var color = document.getElementById("cor");
+            color.style.backgroundColor='#ff0000';
+        break;
+
+        case 6:
+            var color = document.getElementById("cor");
+            color.style.backgroundColor='#993399';
+        break;
+
+        case 7:
+            var color = document.getElementById("cor");
+            color.style.backgroundColor='#ffff00';
+        break;
+
+        case 8:
+            var color = document.getElementById("cor");
+            color.style.backgroundColor='#d3d3d3';
+        break;
+
+        case 9:
+            var color = document.getElementById("cor");
+            color.style.backgroundColor='#ff6200';
+        break;
+        
+        default:
+            break;
     }
-*/
+    console.log (miliSec);
+    
+}
