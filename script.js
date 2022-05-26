08730627d464f75256fec673ebcc78cd029cbb61
Anderson Lucho <63527795+Alucho@users.noreply.github.com>
2022-05-26 12:40:12 -0300

    update

--- script.js
+++ script.js
@@ -1,60 +1,22 @@
 function clicktime(){
-
+  
+    var color = document.getElementById("cor");
     var miliSec = Date.now()%10;
     switch (miliSec) {
-        case 0:
-            var color = document.getElementById("cor");
-            color.style.backgroundColor='#ffffff';
-        break;
-
-        case 1:
-            var color = document.getElementById("cor");
-            color.style.backgroundColor='#000000';
-        break;
-
-        case 2:
-            var color = document.getElementById("cor");
-            color.style.backgroundColor='#0000ff';
-        break;
-
-        case 3:
-            var color = document.getElementById("cor");
-            color.style.backgroundColor='#008000';
-        break;
-
-        case 4:
-            var color = document.getElementById("cor");
-            color.style.backgroundColor='#ffc0cb';
-        break;
-
-        case 5:
-            var color = document.getElementById("cor");
-            color.style.backgroundColor='#ff0000';
-        break;
-
-        case 6:
-            var color = document.getElementById("cor");
-            color.style.backgroundColor='#993399';
-        break;
-
-        case 7:
-            var color = document.getElementById("cor");
-            color.style.backgroundColor='#ffff00';
-        break;
-
-        case 8:
-            var color = document.getElementById("cor");
-            color.style.backgroundColor='#d3d3d3';
-        break;
-
-        case 9:
-            var color = document.getElementById("cor");
-            color.style.backgroundColor='#ff6200';
-        break;
+        
+        case 0:color.style.backgroundColor='#ffffff';break;
+        case 1:color.style.backgroundColor='#000000';break;
+        case 2:color.style.backgroundColor='#0000ff';break;
+        case 3:color.style.backgroundColor='#008000';break;
+        case 4:color.style.backgroundColor='#ffc0cb';break;
+        case 5:color.style.backgroundColor='#ff0000';break;
+        case 6:color.style.backgroundColor='#993399';break;
+        case 7:color.style.backgroundColor='#ffff00';break;
+        case 8:color.style.backgroundColor='#d3d3d3';break;
+        case 9:color.style.backgroundColor='#ff6200';break;
         
         default:
             break;
     }
     console.log (miliSec);
-    
 }
